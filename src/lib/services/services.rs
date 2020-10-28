7eb94346e94f7f4664562d1f623493446fbe7a7a
Woomy4680-exe <matzar1808@gmail.com>
2020-10-28 18:28:36 +0100

    Updated errors

--- src/lib/services/services.rs
+++ src/lib/services/services.rs
@@ -1,33 +1,45 @@
 use std::process::Command;
 pub fn start_srv (service: String) {
-    let optput = Command::new("rc-service")
-        .args(&[&service, "start"])
-        .spawn()
-        .unwrap()
-        .stdout;
-        println!("{:?}", optput);
+    match Command::new("rc-service").args(&[&service, "start"]).spawn().unwrap() {
+        Ok(out) => {
+            println!("");
+        }
+        Err(e) => {
+            println!("{}", console::style(&format!("Unable to start service {}", service)).red());
+            std::process::exit(1);
+        }
+    }
 }
 pub fn stop_srv (service: String) {
-    let optput = Command::new("rc-service")
-        .args(&[&service, "stop"])
-        .spawn()
-        .unwrap()
-        .stdout;
-        println!("{:?}", optput);
+    match Command::new("rc-service").args(&[&service, "stop"]).spawn().unwrap() {
+        Ok(out) => {
+            println!("");
+        }
+        Err(e) => {
+            println!("{}", console::style(&format!("Unable to stop service {}", service)).red());
+            std::process::exit(1);
+        }
+    }
 }
 pub fn enable_srv (service: String) {
-    let optput = Command::new("rc-update")
-        .args(&["add", &service])
-        .spawn()
-        .unwrap()
-        .stdout;
-        println!("{:?}", optput);
+    match Command::new("rc-update").args(&["add", &service, "default"]).spawn().unwrap() {
+        Ok(out) => {
+            println!("");
+        }
+        Err(e) => {
+            println!("{}", console::style(&format!("Unable to enable service {}", service)).red());
+            std::process::exit(1);
+        }
+    }
 }
 pub fn disable_srv (service: String) {
-    let optput = Command::new("rc-update")
-        .args(&["add", &service])
-        .spawn()
-        .unwrap()
-        .stdout;
-        println!("{:?}", optput);
+    match Command::new("rc-update").args(&["del", &service, "default"]).spawn().unwrap() {
+        Ok(out) => {
+            println!("");
+        }
+        Err(e) => {
+            println!("{}", console::style(&format!("Unable to disable service {}", service)).red());
+            std::process::exit(1);
+        }
+    }
 }
